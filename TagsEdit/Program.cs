1ce6deb074e24253227fe2370d8a01d773329e69
Александр Безгин <bezgin.sasha06@gmail.com>
2017-10-09 16:55:38 +0700

    My first commit

---
+++ TagsEdit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsEdit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var neval = TagLib.File.Create("D:/MusicTest/neval.mp3");
+            Console.WriteLine(neval.Tag.Album);
+            Console.ReadKey();
+        }
+    }
+}
